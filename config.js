ea0579cd597f818a8b9f53fcd75383c02c311dfe
Oliver Oszko <oliver@oszko.net>
2015-03-27 23:28:31 +0100

    Copy from Thrre_es6

---
+++ config.js
@@ -0,0 +1,14 @@
+System.config({
+  "baseURL": "/",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "three": "github:mrdoob/three.js@master"
+  }
+});
+
